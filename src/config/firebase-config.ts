f4b5a1f851c164f2ef4d8153e8928064b219bb5c
launchpad5682 <launchpad5682@gmail.com>
2022-07-06 21:20:54 +0530

    chore: adding firebase for firestore database

---
+++ src/config/firebase-config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAsfdsHMsi1djL0iDogmjB1lNeG2psp9Js",
+  authDomain: "vocally-take-home.firebaseapp.com",
+  projectId: "vocally-take-home",
+  storageBucket: "vocally-take-home.appspot.com",
+  messagingSenderId: "67072211787",
+  appId: "1:67072211787:web:b3eb139244d5f4264906ce",
+  measurementId: "G-7WNPZ46QFL",
+};
+
